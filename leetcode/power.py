c380492ff3146bb840126d87b62f7e0b3cdafc48
Daniel Frystacky <danielfrystacky@gmail.com>
2020-05-29 10:27:11 -0400

    warm up problems

---
+++ leetcode/power.py
@@ -0,0 +1,12 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        if n < 0:
+            return self.myPow(1/x, -n)
+        if n == 0:
+            return 1
+        if n % 2 == 0:
+            return self.myPow(x*x, n//2)
+        else:
+            return x * self.myPow(x*x, (n-1)//2)
+        
